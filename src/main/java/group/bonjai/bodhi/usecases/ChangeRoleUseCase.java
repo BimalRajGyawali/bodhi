35c47a81f3a22e5b891880f569a3b67693459b5d
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-07-12 23:29:45 +0545

    adds change role use case

--- src/main/java/group/bonjai/bodhi/usecases/ChangeRoleUseCase.java
+++ src/main/java/group/bonjai/bodhi/usecases/ChangeRoleUseCase.java
@@ -1,10 +1,12 @@
 package group.bonjai.bodhi.usecases;
 
+import group.bonjai.bodhi.exceptions.ConstraintViolation;
 import group.bonjai.bodhi.exceptions.ResourceNotFound;
+import group.bonjai.bodhi.models.DepartmentMember;
 import group.bonjai.bodhi.repositories.DepartmentMemberRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
+import java.util.*;
 
 @Component
 public class ChangeRoleUseCase implements IChangeRoleUseCase{
@@ -14,11 +16,40 @@ public class ChangeRoleUseCase implements IChangeRoleUseCase{
         this.departmentMemberRepository = departmentMemberRepository;
     }
 
+    /**
+     * The role of only Teacher or Assistant can be changed through this use case.
+     *
+     * Role of HOD cannot be changed through this use case. To change role of HOD, one must go through
+     * `appoint a new HOD` use case.
+     *
+     * Role of any department member cannot be changed to HOD. For this also, one must go through
+     * `appoint a new HOD` use case.
+     *
+     * The new role of Teacher or Assistant must be again Teacher or Assistant. A Teacher can be changed to Assistant
+     * or an Assistant can be changed to Teacher through this use case.
+     */
     @Override
-    public void execute(UUID departmentMemberId, String newRole) throws ResourceNotFound{
-        if(!departmentMemberRepository.existsById(departmentMemberId)){
+    public void execute(UUID departmentMemberId, String newRole)
+            throws ResourceNotFound, ConstraintViolation {
+
+        final Set<String> permittedRoles = new HashSet<>
+                (Arrays.asList(DepartmentMember.TEACHER, DepartmentMember.ASSISTANT));
+
+        Optional<DepartmentMember> optionalDepartmentMember = departmentMemberRepository.findById(departmentMemberId);
+
+        if(!optionalDepartmentMember.isPresent()){
             throw new ResourceNotFound(departmentMemberId);
         }
+        DepartmentMember departmentMember = optionalDepartmentMember.get();
 
+        if(!permittedRoles.contains(newRole)){
+          throw new ConstraintViolation("role", "newRole must be in "+permittedRoles);
+        }
+
+        if(!permittedRoles.contains(departmentMember.getRole())){
+            throw new ConstraintViolation("role", "role of department member must be in "+permittedRoles);
+        }
+        departmentMember.setRole(newRole);
+        departmentMemberRepository.save(departmentMember);
     }
 }
